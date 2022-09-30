c53050dd19773c05c6358e237aa22683dbe51ac1
amey1601 <98279143+amey1601@users.noreply.github.com>
2022-09-30 22:39:23 +0530

    Add files via upload

---
+++ Graded Assignment/src/com/greatlearning/departments/adminDepartment.java
@@ -0,0 +1,21 @@
+package gl.adminDepartment;
+
+
+import superdeaprt.superDepartment;
+public class adminDepartment extends superDepartment {
+	public String departmentName() {
+		return "Admin Department";
+	}
+	
+	public String getTodaysWork() {
+		return "Complete your documents Submission";
+	}
+	
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	
+}
+
+
